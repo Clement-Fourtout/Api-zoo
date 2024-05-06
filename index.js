c9322db5c00c839fa8956d645b32c98099922a47
clement <clems.corsica70@gmail.com>
2024-05-06 17:47:49 +0200

    add auth

--- index.js
+++ index.js
@@ -2,57 +2,48 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { Pool } = require('pg');
 
 const PORT = process.env.PORT || 3000;
 
 // Middleware pour parser le JSON
 app.use(express.json());
 
-app.use(cors({
+const corsOptions = {
   origin: 'https://zoo-arcadia-31989dc8c54b.herokuapp.com',
   optionsSuccessStatus: 200 // Certains navigateurs peuvent exiger une option de statut de succès explicite
-}));
+};
 
-// Configuration de la connexion à la base de données PostgreSQL
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Utilisez la variable d'environnement DATABASE_URL pour la connexion à Heroku PostgreSQL
-  ssl: {
-    rejectUnauthorized: false // Autoriser les connexions SSL non autorisées pour Heroku
-  }
-});
+app.use(cors(corsOptions));
+// Données de démonstration - liste de tâches
+let tasks = [
+  { id: 1, title: 'Etat de santé :' },
+  { id: 2, title: 'Nourriture :' },
+  { id: 3, title: 'Grammage :' },
+  { id: 4, title: 'Date de passage :' },
+];
 
 // Route de connexion
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
+  // Votre logique d'authentification ici
+  // Par exemple, vérifier si les identifiants sont valides dans une base de données
+  const user = await User.findOne({ nom: username });
 
-  try {
-    // Recherchez l'utilisateur dans la base de données par nom d'utilisateur
-    const query = 'SELECT * FROM utilisateurs WHERE nom = $1';
-    const result = await pool.query(query, [username]);
-
-    if (result.rows.length > 0) {
-      const user = result.rows[0];
-      // Si l'utilisateur est trouvé, vérifiez le mot de passe
-      const match = await bcrypt.compare(password, user.mot_de_passe);
-      
-      if (match) {
-        // Si le mot de passe correspond, générez un jeton JWT et renvoyez-le
-        const token = jwt.sign({ username }, 'votre_clé_secrète', { expiresIn: '1h' });
-        res.json({ token });
-      } else {
-        // Si le mot de passe ne correspond pas, renvoyez une erreur d'authentification
-        res.status(401).json({ message: 'Nom d\'utilisateur ou mot de passe incorrect' });
-      }
+  if (user) {
+    // Si l'utilisateur est trouvé, vérifiez le mot de passe
+    const match = await bcrypt.compare(password, user.mot_de_passe);
+    
+    if (match) {
+      // Si le mot de passe correspond, générez un jeton JWT et renvoyez-le
+      const token = jwt.sign({ username }, 'votre_clé_secrète', { expiresIn: '1h' });
+      res.json({ token });
     } else {
-      // Si l'utilisateur n'est pas trouvé, renvoyez une erreur d'authentification
+      // Si le mot de passe ne correspond pas, renvoyez une erreur d'authentification
       res.status(401).json({ message: 'Nom d\'utilisateur ou mot de passe incorrect' });
     }
-  } catch (error) {
-    // Gérer les erreurs de base de données
-    console.error('Erreur de base de données :', error);
-    res.status(500).json({ message: 'Erreur de base de données' });
+  } else {
+    // Si l'utilisateur n'est pas trouvé, renvoyez une erreur d'authentification
+    res.status(401).json({ message: 'Nom d\'utilisateur ou mot de passe incorrect' });
   }
 });
 
