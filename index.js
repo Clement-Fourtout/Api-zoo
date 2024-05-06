9821be6b6146343636f7511b7348afb42034bad4
clement <clems.corsica70@gmail.com>
2024-05-06 18:11:51 +0200

    add auth

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const { Pool } = require('pg');
 const PORT = process.env.PORT || 3000;
 
