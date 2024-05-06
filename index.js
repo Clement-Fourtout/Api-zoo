77812f9c0cddab0928becd1d9453e42c9ccd2422
clement <clems.corsica70@gmail.com>
2024-05-06 17:00:25 +0200

    add auth

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+
+
 const PORT = process.env.PORT || 3000;
 
 // Middleware pour parser le JSON
@@ -21,49 +21,6 @@ let tasks = [
   { id: 3, title: 'Grammage :' },
   { id: 4, title: 'Date de passage :' },
 ];
-
-// Récupérer toutes les tâches
-app.get('/tasks', (req, res) => {
-  res.json(tasks);
-});
-
-// Récupérer une tâche par son ID
-app.get('/tasks/:id', (req, res) => {
-  const taskId = parseInt(req.params.id);
-  const task = tasks.find(task => task.id === taskId);
-  if (task) {
-    res.json(task);
-  } else {
-    res.status(404).json({ message: 'Tâche non trouvée' });
-  }
-});
-
-// Ajouter une nouvelle tâche
-app.post('/tasks', (req, res) => {
-  const { title } = req.body;
-  const newTask = { id: tasks.length + 1, title };
-  tasks.push(newTask);
-  res.status(201).json(newTask);
-});
-
-// Mettre à jour une tâche existante
-app.put('/tasks/:id', (req, res) => {
-  const taskId = parseInt(req.params.id);
-  const task = tasks.find(task => task.id === taskId);
-  if (task) {
-    task.title = req.body.title;
-    res.json(task);
-  } else {
-    res.status(404).json({ message: 'Tâche non trouvée' });
-  }
-});
-
-// Supprimer une tâche
-app.delete('/tasks/:id', (req, res) => {
-  const taskId = parseInt(req.params.id);
-  tasks = tasks.filter(task => task.id !== taskId);
-  res.status(204).end();
-});
 
 // Route de connexion
 app.post('/login', (req, res) => {
