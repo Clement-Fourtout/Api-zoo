f54c6c687c246e5fc048360bd9698c1d9ada82db
clement <clems.corsica70@gmail.com>
2024-05-06 19:29:25 +0200

    passport

--- Passport.js
+++ Passport.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const { Pool } = require('pg');
@@ -21,17 +22,23 @@ app.use(passport.initialize());
 
 // Configuration de la stratégie de nom d'utilisateur et de mot de passe
 passport.use(new LocalStrategy({
-    usernameField: 'username',
-    passwordField: 'password'
+    usernameField: 'nom',
+    passwordField: 'mot_de_passe'
 }, async (username, password, done) => {
     try {
         // Requête pour trouver l'utilisateur dans la base de données par nom d'utilisateur
         const query = 'SELECT * FROM utilisateurs WHERE nom_utilisateur = $1';
         const { rows } = await pool.query(query, [username]);
 
-        // Si aucun utilisateur correspondant n'est trouvé ou si le mot de passe est incorrect
-        if (rows.length === 0 || rows[0].mot_de_passe !== password) {
-            return done(null, false, { message: 'Nom d\'utilisateur ou mot de passe incorrect' });
+        // Si aucun utilisateur correspondant n'est trouvé
+        if (rows.length === 0) {
+            return done(null, false, { message: 'Nom d\'utilisateur incorrect' });
+        }
+
+        // Vérifier le hachage du mot de passe avec Bcrypt
+        const match = await bcrypt.compare(password, rows[0].mot_de_passe);
+        if (!match) {
+            return done(null, false, { message: 'Mot de passe incorrect' });
         }
 
         // Si l'utilisateur est trouvé et que le mot de passe est correct
