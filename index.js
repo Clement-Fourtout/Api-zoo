68140ad8e486350c0e45e6984cea6fe027c19995
clement <clems.corsica70@gmail.com>
2024-05-06 18:47:13 +0200

    add auth

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-
 const { Pool } = require('pg');
 const PORT = process.env.PORT || 3000;
 
@@ -73,21 +72,21 @@ app.delete('/tasks/:id', (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+  const { nom, mot_de_passe  } = req.body;
 
   try {
     // Recherchez l'utilisateur dans la base de données par nom d'utilisateur
     const query = 'SELECT * FROM utilisateurs WHERE nom = $1';
-    const result = await pool.query(query, [username]);
+    const result = await pool.query(query, [nom]);
 
     if (result.rows.length > 0) {
       const user = result.rows[0];
       // Si l'utilisateur est trouvé, vérifiez le mot de passe
-      const match = await bcrypt.compare(password, user.mot_de_passe);
+      const match = await bcrypt.compare(mot_de_passe , user.mot_de_passe);
       
       if (match) {
         // Si le mot de passe correspond, générez un jeton JWT et renvoyez-le
-        const token = jwt.sign({ username }, 'votre_clé_secrète', { expiresIn: '1h' });
+        const token = jwt.sign({ nom }, 'votre_clé_secrète', { expiresIn: '1h' });
         res.json({ token });
       } else {
         // Si le mot de passe ne correspond pas, renvoyez une erreur d'authentification
