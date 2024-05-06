82bd88366687bfb8b832aca5de51b7b4333c4170
clement <clems.corsica70@gmail.com>
2024-05-06 17:57:38 +0200

    add auth

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const PORT = process.env.PORT || 3000;
 
 // Middleware pour parser le JSON
