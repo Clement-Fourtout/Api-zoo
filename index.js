2d242c25fb2bd013cd73a7b6921007bc097f9d8b
clement <clems.corsica70@gmail.com>
2024-05-10 19:52:13 +0200

    jawsdb

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.post('/login', async (req, res) => {
               const user = result[0];
               console.log('Mot de passe haché récupéré depuis la base de données :', user.mot_de_passe);
               const match = await bcrypt.compare(mot_de_passe, user.mot_de_passe);
-              console.log('mot de passe:',{mot_de_passe});
+              console.log('mot de passe:', mot_de_passe);
 
               if (match) {
                   // Le mot de passe saisi par l'utilisateur correspond au mot de passe haché dans la base de données
