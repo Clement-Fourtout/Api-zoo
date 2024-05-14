d6de8279563ed2436a833aed3be49df998b238c1
clement <clems.corsica70@gmail.com>
2024-05-14 20:30:37 +0200

    delete

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.put('/tasks/:id', (req, res) => {
 });
 
 // Supprimer une tâche
-app.delete('/user', (req, res) => {
+app.delete('/user/:userId', (req, res) => {
     const userId = req.params.userId;
 
     // Requête pour supprimer l'utilisateur de la base de données
