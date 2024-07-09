ac8a4aa9222d26772d792cb48334f611cbe2d9ff
clement <clems.corsica70@gmail.com>
2024-07-09 17:02:40 +0200

    aws-sdk

--- index.js
+++ index.js
@@ -40,24 +40,23 @@ const corsOptions = {
 };
 
 AWS.config.update({
-    region: process.env.AWS_REGION,
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
 });
 
 const s3 = new AWS.S3();
-
-// Configuration de multer pour utiliser S3
+  
 const upload = multer({
     storage: multerS3({
         s3: s3,
         bucket: process.env.AWS_S3_BUCKET,
-        acl: 'public-read', // ou 'public-read' selon vos besoins
         key: function (req, file, cb) {
-            cb(null, file.originalname);
-        }
-    })
+            cb(null, Date.now().toString() + '-' + file.originalname);
+        },
+    }),
 });
+
 
 
 app.use(cors());
