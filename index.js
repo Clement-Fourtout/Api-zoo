5e6f34e00350953bca6d50e50bd4e5dac323ea90
clement <clems.corsica70@gmail.com>
2024-07-09 17:19:36 +0200

    aws-sdk

--- index.js
+++ index.js
@@ -39,13 +39,12 @@ const corsOptions = {
     allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
 };
 
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+const s3 = new AWS.S3({
     region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
-const s3 = new AWS.S3();
   
 const upload = multer({
     storage: multerS3({
