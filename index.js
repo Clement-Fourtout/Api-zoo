3a28e7e852b246901b94375fa466cb3e42c4f0ff
clement <clems.corsica70@gmail.com>
2024-07-10 18:39:39 +0200

    add Habitats

--- index.js
+++ index.js
@@ -513,7 +513,7 @@ app.get('/habitats', (req, res) => {
             res.status(500).json({ error: 'Erreur serveur lors de la récupération des habitats' });
         } else {
             // Ajuster chaque résultat pour inclure l'URL complète de l'image
-            const habitatsWithImageUrl = habitats.map(habitat => {
+            const habitatsWithImageUrl = result.map(habitat => {
                 return {
                     ...habitat,
                     image: habitat.image // Assurez-vous que habitat.image est déjà une URL complète
